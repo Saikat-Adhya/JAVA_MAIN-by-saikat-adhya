46906215befd066adc881300106f1b1e191c4753
Saikat-Adhya <saikatadhya1@gmail.com>
2024-08-19 17:22:40 +0530

    Some Changes

---
+++ DSAPratice_TopiceWise/Arrays1/SorttheFirstPartassendingorderandotherPartDesndingorder.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SorttheFirstPartassendingorderandotherPartDesndingorder {
+
+    static void questionWise(int[]arr){
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length/2; i++) {
+            int s=arr[i];
+            System.out.print(s+" ");             //This is for the first part assending order
+        }
+
+        for (int j = arr.length - 1; j >= arr.length / 2; j--)
+      {
+         System.out.print(arr[j] + " ");
+      }
+
+    }
+    public static void main(String[] args) {
+        int[]arr={3, 2, 4, 1, 10, 30, 40, 20,90,100};
+        questionWise(arr);
+    }
+    
+}
