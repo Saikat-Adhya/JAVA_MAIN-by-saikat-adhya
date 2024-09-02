62ee37b80eb22345dacbad6123a6f022b563135d
Saikat-Adhya <saikatadhya1@gmail.com>
2024-09-02 17:23:50 +0530

    ok

--- Previous_MainPratice/Methods.java
+++
@@ -1,9 +0,0 @@
-public class Methods {
-    public static void main(String[] args) {
-        Methods m =new Methods();
-        m.sum();
-    }
-    void sum(){
-        System.out.println("ddsdddx!!@@");
-    }
-}
